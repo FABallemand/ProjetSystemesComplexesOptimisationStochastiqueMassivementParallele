7b9ddd6dd8207c6f1fe8e06db6f556772f2bbadf
FABallemand <allemand.fabien@orange.fr>
2023-01-11 19:06:53 +0100

    Earth trajectory

--- earth_trajectory/earth_trajectory_test.c
+++ earth_trajectory/earth_trajectory_test.c
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 #include <math.h>
 
+// https://fr.wikipedia.org/wiki/Loi_universelle_de_la_gravitation
+// https://www.futura-sciences.com/sciences/definitions/systeme-solaire-perihelie-2/
+// https://planet-terre.ens-lyon.fr/ressource/orbite-Terre.xml
+// https://forums.futura-sciences.com/archives/708501-calcul-orbite-soleil-terre.html
+
 #define NB_SAMP 1024
 #define DIM 2
 #define R 0
 #define THETA 1
 
-#define SUN_WEIGHT = 1.9884 * pow(10, 30) // Masse du Soleil
-#define G = 6, 67408 * pow(10, -11)       // Constante gravitationnelle
-
 int main()
 {
+    printf("===================================\n");
+    printf("Compute Earth trajectory around Sun\n");
     // Open output file
     FILE *output_file = NULL;
     if ((output_file = fopen("earth_trajectory_output.txt", "w")) == NULL)
@@ -32,11 +36,13 @@ int main()
     int n = 0;                                       //< Current step
 
     // Initial conditions
-    trajectory[n][R] = 147 * pow(10, 6);   //< Initial position (perihelion)
-    trajectory[n][THETA] = M_1_PI;         //< Initial position (perihelion)
-    curr_speed[R] = 0;                     //< Initial speed (perihelion)
-    curr_speed[THETA] = 30.2 * pow(10, 3); //< Initial speed (perihelion)
-    n++;                                   //< Next step
+    trajectory[n][R] = 147.1 * pow(10, 9);                                   //< Initial position (perihelion)
+    trajectory[n][THETA] = M_PI;                                             //< Initial position (perihelion)
+    curr_speed[R] = 0;                                                       //< Initial speed (perihelion)
+    curr_speed[THETA] = 30.2 * pow(10, 3);                                   //< Initial speed (perihelion)
+    curr_speed[THETA] = (2 * M_PI) / (365.5 * 24 * 3600);                    //< Initial speed (average)
+    fprintf(output_file, "%f,%f\n", trajectory[n][R], trajectory[n][THETA]); // Write coord. in output file
+    n++;                                                                     //< Next step
 
     // Compute trajectory
     for (; n < NB_SAMP; n++)
@@ -44,7 +50,7 @@ int main()
         prev_speed[R] = curr_speed[R];
         prev_speed[THETA] = curr_speed[THETA];
 
-        curr_speed[R] = prev_speed[R] + (2 * trajectory[n - 1][R] * prev_speed[THETA] - (g_const * sun_weight) / (trajectory[n - 1][R] * trajectory[n - 1][R])) * delta_t;
+        curr_speed[R] = prev_speed[R] + (trajectory[n - 1][R] * prev_speed[THETA] * prev_speed[THETA] - ((g_const * sun_weight) / (trajectory[n - 1][R] * trajectory[n - 1][R]))) * delta_t;
         curr_speed[THETA] = prev_speed[THETA] + ((-2 * prev_speed[R] * prev_speed[THETA]) / trajectory[n - 1][R]) * delta_t;
 
         trajectory[n][R] = trajectory[n - 1][R] + curr_speed[R] * delta_t;
@@ -55,6 +61,8 @@ int main()
 
     // Close output file
     fclose(output_file);
+
+    printf("===================================\n");
 
     return 0;
 }
--- earth_trajectory/earth_trajectory_vis.py
+++ earth_trajectory/earth_trajectory_vis.py
@@ -7,33 +7,51 @@ import os
 file_directory = os.path.dirname(os.path.realpath(__file__))
 os.chdir(file_directory)
 
-def polToCart(polar_coord):
-    """Convert a list of polar coordinates in two lists of cartesian coordinates
+
+def polToCart(polar_coord_r, polar_coord_theta):
+    """Convert lists of polar coordinates in lists of cartesian coordinates
 
     Args:
-        polar_coord (list): List of polar coordinates
+        polar_coord_r (list): List of polar coordinates r
+        polar_coord_theta (list): List of polar coordinates theta
 
     Returns:
-        list: List of cartesian coordinates
+        list: List of cartesian coordinates x
+        list: List of cartesian coordinates y
     """
-    x = []
-    y = []
-    for coord in polar_coord:
-        r, theta = coord
-        x.append(r * cos(theta))
-        y.append(r * sin(theta))
-    return x, y
+    cart_coord_x = []
+    cart_coord_y = []
+    for i in range(len(polar_coord_r)):
+        r, theta = polar_coord_r[i], polar_coord_theta[i]
+        cart_coord_x.append(r * cos(theta))
+        cart_coord_y.append(r * sin(theta))
+    return cart_coord_x, cart_coord_y
 
-polar_coord = []
+
+polar_coord_r = []
+polar_coord_theta = []
 cart_coord_x = []
 cart_coord_y = []
 
 with open("earth_trajectory_output.txt") as file:
     for line in file:
         r, theta = line.split(',')
-        polar_coord.append((float(r), float(theta)))
+        polar_coord_r.append(float(r))
+        polar_coord_theta.append(float(theta))
 
-cart_coord_x, cart_coord_y = polToCart(polar_coord)
+# plt.plot(range(1024), polar_coord_r)
+# plt.show()
 
-plt.plot(cart_coord_x, cart_coord_y)
+cart_coord_x, cart_coord_y = polToCart(polar_coord_r, polar_coord_theta)
+
+print("Nb points =", len(cart_coord_x))
+print(cart_coord_x[:5], "\n", cart_coord_y[:5])
+
+# plt.scatter(cart_coord_x, cart_coord_y)
+# plt.scatter(cart_coord_x[0], cart_coord_y[0])
+# plt.scatter(cart_coord_x[1023], cart_coord_y[1023])
+plt.plot(cart_coord_x, cart_coord_y, "--")
+plt.scatter([0], [0], color="red")
+plt.xlim(min(cart_coord_x)+100, max(cart_coord_x)+100)
+plt.ylim(min(cart_coord_y)+100, max(cart_coord_y)+100)
 plt.show()
